b061442e9783e50883781eacd58f373e31dc3769
John Antonios <john.antonios@sydney.edu.au>
2020-01-10 14:38:42 +1100

    added stupid build folder for testing

---
+++ build/precache-manifest.3f8af9cbf4fd52a105470e5bd731d84f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df2dda5353baa0cc4458e75f8d5b970",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8385854651541b95806d",
+    "url": "/static/css/main.7faae64d.chunk.css"
+  },
+  {
+    "revision": "52108e0bc96933f3c987",
+    "url": "/static/js/2.1bcbf12b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.1bcbf12b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8385854651541b95806d",
+    "url": "/static/js/main.13d190df.chunk.js"
+  },
+  {
+    "revision": "bac747809ee99ab0058d",
+    "url": "/static/js/runtime-main.1d4f2a7b.js"
+  }
+]);
